cb31a0000997bcdf01b1557b1cdc42314ede8efc
ahmadrosid <alahmadrosid@gmail.com>
2022-03-29 22:34:43 +0700

    chore: tidy up

--- screenshot.sh
+++ screenshot.sh
@@ -6,17 +6,20 @@ DEV_PORT="$(lsof -ti tcp:3000)"
 
 if [ -z "${DEV_PORT}" ]
 then
-    echo "start dev server"
-    yarn dev &
+    echo "starting dev server"
+    yarn dev &>/dev/null & disown;
 fi
 
 if [ "$1" == "potrait" ]
 then
+    echo "screenshot potrait image"
     firefox --no-remote --window-size 1080,1920 --screenshot http://localhost:3000/potrait
     mv screenshot.png potrait.png
 else
+    echo "screenshot landscape image"
     firefox --screenshot http://localhost:3000
     mv screenshot.png landscape.png
 fi
 
-kill $DEV_PORT
+echo "kill dev server pid:$DEV_PORT"
+lsof -ti tcp:3000 | xargs kill 
